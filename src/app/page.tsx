7072d4eebdbabfdc019bb6ae726ca6dfbb552094
Bruno <33235089+brunobold@users.noreply.github.com>
2024-01-21 13:39:10 +0900

    Initial

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 id="screen-gameboy-text">soon™</h1>
+      </div>
+    </main>
+  );
+}
