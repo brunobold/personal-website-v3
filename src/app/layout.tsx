fbed7eddf4b65dde4fa52bc0da542929d0e6d659
Bruno <33235089+brunobold@users.noreply.github.com>
2024-01-28 22:14:15 +0900

    Speed Insights fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body className={chivo.className}>
         <Navbar />
         {children}
